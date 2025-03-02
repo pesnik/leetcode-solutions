3a39eaff296e5cdc034efc520fdbc7c913cc0bd6
rhasanm <hasanrakibul.masum@gmail.com>
2025-03-02 21:50:25 +0600

    solution: p1431

--- study-plans/leetcode_75/src/lib.rs
+++ study-plans/leetcode_75/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod p1768_merge_strings_alternately;
 
 pub mod p1071_greatest_common_divisor_of_strings;
+
+pub mod p1431_kids_with_the_greatest_number_of_candies;
---
+++ study-plans/leetcode_75/src/p1431_kids_with_the_greatest_number_of_candies.rs
@@ -0,0 +1,27 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
+        let max: i32 = candies.iter().copied().fold(0i32, i32::max);
+
+        candies.iter().map(|candy| {
+            if candy + extra_candies >= max {
+                true
+            } else {
+                false
+            }
+        })
+        .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        let response: Vec<bool> = Solution::kids_with_candies([2,3,5,1,3].to_vec(), 3);
+        assert_eq!(response, [true, true, true, false, true].to_vec());
+    }
+}
