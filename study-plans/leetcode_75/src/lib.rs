a69c523bbd0d63a8394d11beb628bf61e355d8f0
rhasanm <hasanrakibul.masum@gmail.com>
2025-03-07 21:10:53 +0600

    feat: p283

--- study-plans/leetcode_75/src/lib.rs
+++ study-plans/leetcode_75/src/lib.rs
@@ -15,3 +15,5 @@ pub mod p151_reverse_words_in_a_string;
 pub mod p443_string_compression;
 
 pub mod p334_increasing_triplet_subsequence;
+
+pub mod p283_move_zeroes;
---
+++ study-plans/leetcode_75/src/p283_move_zeroes.rs
@@ -0,0 +1,28 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut nonzeroes = nums
+            .iter()
+            .filter_map(|&num| match num {
+                0 => None,
+                _ => Some(num)
+            })
+            .collect::<Vec<i32>>();
+        let zeroes: Vec<i32> = vec![0; nums.len() - nonzeroes.len()];
+        nonzeroes.extend(zeroes);
+        *nums = nonzeroes;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        let mut nums = vec![0, 1, 0, 3, 12];
+        Solution::move_zeroes(&mut nums);
+        assert_eq!(nums, vec![1, 3, 12, 0, 0]);
+    }
+}
